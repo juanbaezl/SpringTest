59b73c3565db300c0be890b822ad391a3f51af55
juanbaezl <juanbaez695@gmail.com>
2022-07-19 10:00:16 -0400

    first commit

---
+++ src/test/java/co/edu/escuelaing/TestPrimerLive.java
@@ -0,0 +1,14 @@
+package co.edu.escuelaing;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestPrimerLive {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
